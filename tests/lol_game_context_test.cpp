a1b3a1cea17e201bca462cad5dea0eb7b514f668
Brendan Keane <85752216+brendanwilliam@users.noreply.github.com>
2026-08-13 14:09:48 -0700

    fix: collect classic games without queue IDs

--- tests/lol_game_context_test.cpp
+++ tests/lol_game_context_test.cpp
@@ -8,37 +8,18 @@ int main()
 	const QJsonObject supported{
 		{"activePlayer",
 		 QJsonObject{{"riotIdGameName", "Player"}, {"riotIdTagLine", "NA1"}, {"championName", "Ahri"}}},
-		{"gameData", QJsonObject{{"mapNumber", 11},
-					 {"gameMode", "CLASSIC"},
-					 {"gameQueueConfigId", 420},
-					 {"gameTime", 0.0}}}};
+		{"gameData", QJsonObject{{"mapNumber", 11}, {"gameMode", "CLASSIC"}, {"gameTime", 0.0}}}};
 	assert(supported_game(parse_game_context(supported)));
-	for (const auto &queue : {400, 420, 430, 440, 490}) {
-		QJsonObject allowed = supported;
-		QJsonObject game = allowed["gameData"].toObject();
-		game["gameQueueConfigId"] = queue;
-		allowed["gameData"] = game;
-		assert(supported_game(parse_game_context(allowed)));
-	}
-	for (const auto &field : {QString("mapNumber"), QString("gameQueueConfigId")}) {
-		QJsonObject mutated = supported;
-		QJsonObject game = mutated["gameData"].toObject();
-		game[field] = field == "mapNumber" ? 12 : 0;
-		mutated["gameData"] = game;
-		assert(!supported_game(parse_game_context(mutated)));
-	}
+	QJsonObject unsupported_map = supported;
+	QJsonObject game = unsupported_map["gameData"].toObject();
+	game["mapNumber"] = 12;
+	unsupported_map["gameData"] = game;
+	assert(!supported_game(parse_game_context(unsupported_map)));
 	QJsonObject practice_tool = supported;
 	practice_tool["gameData"] = QJsonObject{{"mapNumber", 11},
 						{"gameMode", "PRACTICETOOL"},
 						{"gameQueueConfigId", 0},
 						{"gameTime", 0.0}};
 	assert(supported_game(parse_game_context(practice_tool)));
-	QJsonObject unsupported = supported;
-	unsupported["gameData"] =
-		QJsonObject{{"mapNumber", 11}, {"gameMode", "CLASSIC"}, {"gameQueueConfigId", 999}, {"gameTime", 0.0}};
-	assert(!supported_game(parse_game_context(unsupported)));
-	unsupported["gameData"] =
-		QJsonObject{{"mapNumber", 11}, {"gameMode", "CLASSIC"}, {"queueId", 420}, {"gameTime", 0.0}};
-	assert(!supported_game(parse_game_context(unsupported)));
 	return 0;
 }
